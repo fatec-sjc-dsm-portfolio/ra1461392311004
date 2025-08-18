142db90c6584cf22932c9f1cb55e85871a0841a0
Felipe Vieira <felipe.vieira30@fatec.sp.gov.br>
2025-08-18 15:51:48 -0300

    Primeira versão do portfólio

---
+++ docs/static/js/projetos.js
@@ -0,0 +1,203 @@
+const projetosAcademicos = [
+  {
+    titulo: "Site para consulta de dados sobre Covid longa no Vale do Paraiba.",
+    subtitulo: "",
+    desc: "Atuei como Scrum Master para a API (Aprendizagem por Projetos Integrados) do 1° semestre. O projeto consiste em um site focado em dados relacionados a Covid-19, seus efeitos e seus sintômas prolongados, ou Covid longa de acordo com a Organização Mundial da Saúde (OMS). Desenvolvido entre Mar-Jun/23.",
+    link: "https://github.com/Equipe-CodeLand/API-2023.1",
+    img: "../static/images/capas/api.gif",
+    tec: [
+      "https://cdn-icons-png.flaticon.com/128/5968/5968267.png",
+      "https://cdn-icons-png.flaticon.com/128/5968/5968242.png",
+      "../static/images/icons/python.png",
+      "../static/images/icons/mysql.png",
+      "../static/images/icons/javascript.png"
+    ]
+  },
+  {
+    titulo: "Em breve...",
+    subtitulo: "",
+    desc: "Este projeto ainda não foi adicionado, confira mais tarde.",
+    link: "https://github.com/Equipe-CodeLand/API-2023.2",
+    img: "",
+    tec: [
+      "../static/images/icons/react.png",
+      "../static/images/icons/node.png",
+      "../static/images/icons/typescript.png",
+      "../static/images/icons/mysql.png"
+    ]
+  },
+  {    titulo: "Em breve...",
+    subtitulo: "",
+    desc: "Este projeto ainda não foi adicionado, confira mais tarde.",
+    link: "https://github.com/Equipe-CodeLand/API-2024.1",
+    img: "",
+    tec: [
+          "../static/images/icons/react.png",
+          "https://cdn-icons-png.flaticon.com/128/5968/5968242.png",
+          "../static/images/icons/java.png",
+          "../static/images/icons/spring.png",
+          "../static/images/icons/mysql.png"
+    ]
+  },
+  // {
+  //     titulo: "Sistema para gestão de chamados para uma fornecedora de Internet Fixa.",
+  //     subtitulo: "",
+  //     desc: "Atuei como Product Owner para a API (Aprendizagem por Projetos Integrados) do 2° semestre. O projeto se refere à um sistema de Gestão de Chamadas de Serviço, contendo informações detalhadas sobre a disponibilidade de atendentes, prioridade do chamados e acompanhamento completo no atendimento das chamadas. Desenvolvido entre Ago-Nov/23.",
+  //     link: "https://github.com/Equipe-CodeLand/API-2023.2",
+  //     img: "https://raw.githubusercontent.com/Equipe-CodeLand/API-2023.2/main/docs/cliente_mvp.gif",
+  //     tec: [
+  //         "../static/images/icons/react.png",
+  //         "../static/images/icons/node.png",
+  //         "../static/images/icons/typescript.png",
+  //         "../static/images/icons/mysql.png"
+  //     ]
+  // },
+  // {
+  //     titulo: "Sistema para o gerenciamento de ativos para um empresa privada.",
+  //     subtitulo: "",
+  //     desc: "Atuei como Product Owner para a API (Aprendizagem por Projetos Integrados) do 3° semestre. O objetivo do projeto é o desenvolvimento de uma solução para o gerenciamento de ativos de uma empresa, contendo informações detalhadas sobre ativos, cadastro e histórico de manutenções e dashboard com indicativos relevantes sobre os ativos. Desenvolvido entre Fev-Jun/24.",
+  //     link: "https://github.com/Equipe-CodeLand/API-2024.1",
+  //     img: "../static/images/capas/mvp_api.gif",
+  //     tec: [
+  //         "../static/images/icons/react.png",
+  //         "https://cdn-icons-png.flaticon.com/128/5968/5968242.png",
+  //         "../static/images/icons/java.png",
+  //         "../static/images/icons/spring.png",
+  //         "../static/images/icons/mysql.png"
+  //     ]
+  // }//,
+  //{
+  //     titulo: "Sistema para coleta de dados de um estação metereológica.",
+  //     subtitulo: "",
+  //     desc: "Atuei como Desenvolvedor para a API (Aprendizagem por Projetos Integradosr) do 4° semestre. O objetivo do projeto é o desenvolvimento de um sistema de coleta de dados de estações meteorológicas. A plataforma deve receber as informações da estação metereológica e em caso de emergência mostrar uma notificação alertando a população. Desenvolvido entre Ago-Dez/24.",
+  //     img: "https://github.com/Equipe-CodeLand/API-2024.2/raw/main/docs/mvp-sprint1.gif",
+  //     link: "https://github.com/Equipe-CodeLand/API-2024.2",
+  //     tec: [
+  //         "../static/images/icons/react.png",
+  //         "../static/images/icons/typescript.png",
+  //         "../static/images/icons/node.png",
+  //         "../static/images/icons/python.png",
+  //         "../static/images/icons/aws.png",
+  //         "../static/images/icons/docker.png",
+  //     ]
+  // },
+  // {
+  //     titulo: "Aplicativo mobile para Reporte de Problemas Urbanos.",
+  //     subtitulo: "",
+  //     desc: "Projeto desenvolvido com o objetivo de simplificar o processo de reporte de problemas urbanos, permitindo o usuário relatar um ocorrido de forma rápida e detalhada, com fotos e localização do ocorrido, além de fornecer um acompanhamento do processo de resolução do problema. Produzido entre Set-Dez/24",
+  //     img: '../static/images/capas/aloprefeito.jpg',
+  //     link: "https://github.com/velipefieira/AloPrefeito",
+  //     tec: [
+  //         "../static/images/icons/react.png",
+  //         "../static/images/icons/javascript.png",
+  //         "../static/images/icons/nodejs.png",
+  //         "../static/images/icons/mysql.png"
+  //     ]
+  // },
+  // {
+  //     titulo: "Chatbot para atendimento de uma pizzaria fictícia usando IA.",
+  //     subtitulo: "",
+  //     desc: "Projeto desenvolvido com o objetivo de produzir um chatbot para simplificar o atendimento de uma pizzaria, utilizando modelos de IA para identificar a intenção do usuário e agilizar o atendimento. Produzido entre Fev-Jun/24",
+  //     img: "../static/images/capas/telegrambot.jpg",
+  //     link: "",
+  //     tec: [
+  //         "../static/images/icons/python.png"
+  //     ]
+  // },
+  // {
+  //     titulo: "Sistema para o gerenciamento de um Petshop.",
+  //     subtitulo: "",
+  //     desc: "Projeto desenvolvido o decorrer de 5 atividades práticas para a disciplina Tecnicas de Programação I, utilizando conceitos de Programação orientada a objetos e arquitetura em camadas com React. Desenvolvido entre Out-Dez/23.",
+  //     link: "https://github.com/velipefieira/Ativ-PetLovers",
+  //     img: "../static/images/capas/petlovers.gif",
+  //     tec: [
+  //         "../static/images/icons/react.png",
+  //         "../static/images/icons/node.png",
+  //         "../static/images/icons/typescript.png",
+  //         "../static/images/icons/java.png"
+  //     ]
+  // },
+  // {
+  //     titulo: "Portfólio",
+  //     subtitulo: "",
+  //     desc: "Projeto desenvolvido inicialmente para disciplina Design Digital, o qual será periodicamente atualizado com novos projetos e incrementados com novas tecnologias que forem aprendidas por mim ao decorrer do tempo. Iniciado em Mar/23",
+  //     link: "https://github.com/velipefieira/Portfolio",
+  //     img: "../static/images/capas/portfolio.gif",
+  //     tec: [
+  //         "../static/images/icons/react.png",
+  //         "../static/images/icons/vercel.png"
+  //     ]
+  // },
+  // {
+  //     titulo: "Site de uma universidade fictícia.",
+  //     subtitulo: "",
+  //     desc: "Site de uma universidade fictícia desenvolvido utilizando conceitos básicos de HTML, CSS, Flask e MYSQL, para desafio da disciplina Desenvolvimento WEB I. Desenvolvido entre Mar-Jun/23",
+  //     link: "https://github.com/velipefieira/DesafioUnes",
+  //     img: "../static/images/capas/unes.gif",
+  //     tec: [
+  //         "https://cdn-icons-png.flaticon.com/128/5968/5968267.png",
+  //         "https://cdn-icons-png.flaticon.com/128/5968/5968242.png",
+  //         "../static/images/icons/python.png",
+  //         "../static/images/icons/mysql.png"
+  //     ]
+  //}
+];
+
+const projetosPessoais = [
+  {
+    titulo: "Space Defender",
+    subtitulo: "Jogo desenvolvido para praticar programação em Python.",
+    desc: "Jogo criado no estilo Space Invaders, para praticar programação em Python. Desenvolvido durante Jun/23.",
+    link: "https://github.com/velipefieira/SpaceDefender",
+    img: "../static/images/capas/space.gif",
+    tec: [
+      "../static/images/icons/python.png"
+    ]
+  }
+];
+
+function renderProjetos(lista, containerId) {
+  const container = document.getElementById(containerId);
+  lista.forEach(projeto => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <a class="custom-link">
+        <img src="${projeto.img}" alt="Imagem do projeto">
+        <h5>${projeto.titulo}</h5>
+      </a>
+    `;
+    div.onclick = () => openModal(projeto);
+    container.appendChild(div);
+  });
+}
+
+// Modal
+const modal = document.getElementById("modal");
+modal.classList.add("modal");
+const modalBody = document.getElementById("modal-body");
+const closeBtn = document.querySelector(".close");
+
+function openModal(projeto) {
+  modalBody.innerHTML = `
+    <h4 class="modal-titulo">${projeto.titulo}</h4>
+    <p class="center">
+      ${projeto.link ? `<a href="${projeto.link}" target="_blank">
+        <img src="${projeto.img}" alt="${projeto.titulo}">
+      </a>` : `<img src="${projeto.img}" alt="${projeto.titulo}">`}
+    </p>
+    <p class="modal-conteudo">${projeto.desc}</p>
+    ${projeto.link ? `<a href="${projeto.link}" target="_blank" class="center">Acesse o repositório</a>` : ""}
+    <h5 class="modal-subtitulo">Tecnologias utilizadas:</h5>
+    <div class="technologies">
+      ${projeto.tec.map(t => `<img src="${t}" alt="tec">`).join("")}
+    </div>
+  `;
+  modal.style.display = "block";
+}
+
+closeBtn.onclick = () => modal.style.display = "none";
+window.onclick = (e) => { if (e.target == modal) modal.style.display = "none"; };
+
+// Inicialização
+renderProjetos(projetosAcademicos, "projetosAcademicos");
+renderProjetos(projetosPessoais, "projetosPessoais");
